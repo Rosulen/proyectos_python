2a9d2e8ab30cea17da4df4561d0dea2d992e825e
Rosulen <camilaojeda97@gmail.com>
2021-01-19 19:23:03 -0500

    Se actualizo write_audio.py

--- write_audio.py
+++ write_audio.py
@@ -1,18 +1,16 @@
 import sounddevice as sd
+import matplotlib.pylab as plt
 import scipy.io.wavfile as wavfile 
 import wavio
+import wave
+
 
 fs = 44100  # Sample rate
 seconds = 5  # Duration of recording
 
 myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
 sd.wait()  # Wait until recording is finished
-wavio.write('saludo.wav', myrecording, fs, sampwidth=3)  # Save as WAV file
+wavio.write('sin_titulo.wav', myrecording, fs, sampwidth=3)  # Save as WAV file
 
-## sumar dos audios
-""" 
-rate, data = wavfile.read("alejandro.wav")
-print(rate) 
 
-plt.figure(1)
-plt.plot(data) """
+
